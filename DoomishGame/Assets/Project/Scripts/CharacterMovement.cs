a65e93bec934a9a2b669b90ea13e5e3fbfe0ef57
Pawel <62904551+Pawiszon15@users.noreply.github.com>
2022-08-13 11:34:29 +0200

    Improved character movement

--- DoomishGame/Assets/Project/Scripts/CharacterMovement.cs
+++ DoomishGame/Assets/Project/Scripts/CharacterMovement.cs
@@ -7,13 +7,15 @@ public class CharacterMovement : MonoBehaviour
 
     Rigidbody2D rb2D = null;
 
-    private float movementSpeed = 3f;
+    [SerializeField] private float maxMovementSpeed;
+    [SerializeField] private float accelerationSpeed;
+
     private float moveHorizontal;
     private float moveVertical;
-    
-    private float jumpForce = 50f;
-    private bool isJumping;
-    private int maxNumberOfJumps;
+
+    [SerializeField] private int maxNumberOfJumps;
+    [SerializeField] private float jumpForce;
+    private bool isGrounded;
     private int jumpsAvailable;
 
 
@@ -23,39 +25,43 @@ public class CharacterMovement : MonoBehaviour
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
 
-        movementSpeed = 3f;
-        jumpForce = 10f;
-        isJumping = true;
-/*        maxNumberOfJumps = 2;
-        jumpsAvailable = 2;*/
+        isGrounded = true;
+
+        maxNumberOfJumps = 2;
+        jumpsAvailable = maxNumberOfJumps;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
-        Debug.Log(isJumping);
-
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("space was hit");
-            if (isJumping == false)
+        {            
+            if(jumpsAvailable>0)
             {
                 rb2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                isJumping = true;
-            }
+                jumpsAvailable = jumpsAvailable - 1;
+                Debug.Log(jumpsAvailable);
+            }  
         }
-
     }
 
     void FixedUpdate()
     {
+        /*//Physical movement
         if (moveHorizontal > 0.1f || moveHorizontal < 0.1f)
         {
-            rb2D.AddForce(new Vector2(moveHorizontal * movementSpeed, 0), ForceMode2D.Impulse);
-        }
+            if (rb2D.velocity.x < maxMovementSpeed && rb2D.velocity.x > -maxMovementSpeed)
+            {
+                rb2D.AddForce(new Vector2(moveHorizontal * movementSpeed, 0), ForceMode2D.Force);
+            }
+        }*/
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal  * maxMovementSpeed, rb2D.velocity.y);
+        rb2D.velocity = Vector2.Lerp(rb2D.velocity, movement, accelerationSpeed);
+
     }
 
 
@@ -64,7 +70,8 @@ public class CharacterMovement : MonoBehaviour
         if(collision.gameObject.tag == "Platform")
         {
             Debug.Log("hitting ground");
-            isJumping = false;
+            isGrounded = true;
+            jumpsAvailable = maxNumberOfJumps;
         }
     }
 
@@ -72,7 +79,7 @@ public class CharacterMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Platform")
         {
-            isJumping = true;
+            isGrounded = false;
         }
     }
 

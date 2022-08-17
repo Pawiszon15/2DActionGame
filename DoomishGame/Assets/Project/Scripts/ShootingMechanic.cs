730fd975c9c28d606b06362d1444597a91033505
Pawel <62904551+Pawiszon15@users.noreply.github.com>
2022-08-17 19:43:17 +0200

    addded TMP, added changing weapons mechanic, added new weapon (dash), to fix next time not turning off collider of the player

--- DoomishGame/Assets/Project/Scripts/ShootingMechanic.cs
+++ DoomishGame/Assets/Project/Scripts/ShootingMechanic.cs
@@ -1,62 +1,101 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootingMechanic : MonoBehaviour
 {
+    [Header("Shotgun")]
     [SerializeField] private float recoilForce;
     [SerializeField] private float timeBetweenShoots;
     [SerializeField] private float speedOfBullets;
     [SerializeField] private float timeOnScreenBullets;
 
-    private bool canShoot;
-    
     [SerializeField] Transform firePoint1;
     [SerializeField] Transform firePoint2;
     [SerializeField] Transform firePoint3;
     [SerializeField] Transform gunPivot;
 
-
     [SerializeField] AudioClip shootSound;
     [SerializeField] GameObject shootgunBullet;
+    [SerializeField] Canvas canvas;
 
-    public Vector2 playerVelocity;
+    [Header("MeleeAttack")]
+    [SerializeField] float DashForce;
+    [SerializeField] float durationOfDash;
+    [SerializeField] float timeBetweenDashes;
+
+    //Shotgun private variable
+    private bool canShoot;
+    private bool usingShotgun;
+
+    //Dash private variable
+    private bool canMelee;
+    private CircleCollider2D circleCollider;
+
+    //General variables
     private Rigidbody2D rb2d;
-
-    // Start is called before the first frame update
+    
+    
     void Start()
     {
+        
+        usingShotgun = true;
         canShoot = true;
+        canMelee = true;
+
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse1) && canShoot)
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            ChangeActiveWeapon();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Mouse1) && canShoot && usingShotgun)
         {
             ShootShotgun();       
         }         
+        
+        if(Input.GetKeyDown(KeyCode.Mouse1) && canMelee && !usingShotgun)
+        {
+            MeleeAttack();
+        }    
     }
 
+
+
+    //Main functionalites
     void ShootShotgun()
     {
         GameObject bullet1 = Instantiate(shootgunBullet, firePoint1.position, firePoint1.rotation);
         GameObject bullet2 = Instantiate(shootgunBullet, firePoint2.position, firePoint1.rotation);
         GameObject bullet3 = Instantiate(shootgunBullet, firePoint3.position, firePoint1.rotation);
-        Recoil();
+        ShotgunRecoil();
         canShoot = false;
+
         StartCoroutine(ShootingCooldown());
     }
 
-    void Recoil()
+    void MeleeAttack()
     {
-        //need direction
-        Vector2 recoilDir = new Vector2(gunPivot.position.x, gunPivot.position.y);
+        rb2d.AddForce(gunPivot.transform.right * DashForce, ForceMode2D.Impulse);
+        circleCollider.isTrigger = true;
+        canMelee = false;
 
-        //need to add impluse force
+        StartCoroutine(DashCooldown());
+    }
+
+
+
+    //Supporting function to shotting and dash
+    void ShotgunRecoil()
+    {
+        //Add impulse in opposite direction of shoot
         rb2d.AddForce(-gunPivot.transform.right * recoilForce, ForceMode2D.Impulse);
-       
     }
 
     void BulletSpread()
@@ -64,10 +103,35 @@ public class ShootingMechanic : MonoBehaviour
         //LATER
     }
 
+    void ChangeActiveWeapon()
+    {
+        if (usingShotgun)
+        {
+            usingShotgun = false;
+            canvas.enabled = false;
+            Debug.Log("isShotgunActive" + usingShotgun);
+        }
 
+        else
+        {
+            usingShotgun = true;
+            canvas.enabled = true;
+            Debug.Log("isShotgunActive" + usingShotgun);
+        }
+    }
+
+
+
+    //Couroutines
     IEnumerator ShootingCooldown()
     {
         yield return new WaitForSeconds(timeBetweenShoots);
         canShoot = true;
     }
+
+    IEnumerator DashCooldown()
+    {
+        yield return new WaitForSeconds(timeBetweenDashes);
+        canMelee = true;
+    }    
 }

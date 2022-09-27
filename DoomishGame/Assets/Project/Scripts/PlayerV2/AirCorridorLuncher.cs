b740b41d0900bd13d7776ee4634d81dfda92e275
Pawel <62904551+Pawiszon15@users.noreply.github.com>
2022-09-27 18:45:22 +0200

    Added rotating air corridor depending on position of spawn

--- DoomishGame/Assets/Project/Scripts/PlayerV2/AirCorridorLuncher.cs
+++ DoomishGame/Assets/Project/Scripts/PlayerV2/AirCorridorLuncher.cs
@@ -12,10 +12,12 @@ public class AirCorridorLuncher : MonoBehaviour
 
     [Header("References")]
     [SerializeField] GameObject weapon;
+    [SerializeField] GameObject firePoint1;
+    [SerializeField] GameObject firePoint2;
     [SerializeField] GameObject airCorridor;
-    
+    [SerializeField] GameObject player;
+
     private ItemSwaper itemSwaper;
-    private Camera camera;
     private float distanceOfRayCast = 1000f;
 
     private void Awake()
@@ -36,15 +38,28 @@ public class AirCorridorLuncher : MonoBehaviour
     private void ChosePlaceOfSpawn()
     {
         Vector2 placeOfSpawn;
-        RaycastHit2D hit = Physics2D.Linecast(weapon.transform.position, weapon.transform.right * distanceOfRayCast);
-        placeOfSpawn = hit.point;
-        StartCoroutine(UseOfCorridorLuncher(placeOfSpawn));
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        float corridorRotation;
+        
+
+        RaycastHit2D hit = Physics2D.Linecast(firePoint1.transform.position, firePoint1.transform.right * distanceOfRayCast);
+        RaycastHit2D hit2 = Physics2D.Linecast(firePoint2.transform.position, firePoint2.transform.right * distanceOfRayCast);
+
+        placeOfSpawn = hit2.point;
+
+        var hitX_1 =Mathf.Round(hit.point.x * 10f) * 0.1f; var hitY_1 = Mathf.Round(hit.point.y * 10f) *0.1f; 
+        var hitX_2 = Mathf.Round(hit2.point.x * 10f) * 0.1f; var hitY_2 = Mathf.Round(hit2.point.y * 10f) * 0.1f;
+       
+        if (hitY_1 == hitY_2 && playerPos.y < hitY_2) {corridorRotation = 180f; StartCoroutine(UseOfCorridorLuncher(placeOfSpawn, corridorRotation));}
+        if (hitX_1 == hitX_2 && playerPos.x <= hitX_2) { corridorRotation = 90f; StartCoroutine(UseOfCorridorLuncher(placeOfSpawn, corridorRotation));}
+        if (hitX_1 == hitX_2 && playerPos.x > hitX_2) { corridorRotation = 270f; StartCoroutine(UseOfCorridorLuncher(placeOfSpawn, corridorRotation));}
+        else { corridorRotation = 0f; StartCoroutine(UseOfCorridorLuncher(placeOfSpawn, corridorRotation)); }
     }
 
-    IEnumerator UseOfCorridorLuncher(Vector2 corridorSpawnPos)
+    IEnumerator UseOfCorridorLuncher(Vector2 corridorSpawnPos, float corridorRotation)
     {
         yield return new WaitForSeconds(setupTime);
-        Instantiate(airCorridor, corridorSpawnPos, Quaternion.identity);
+        Instantiate(airCorridor, corridorSpawnPos, Quaternion.Euler(gameObject.transform.rotation.x, gameObject.transform.rotation.y, corridorRotation));
         itemSwaper.StartCooldown(cooldown);
     }
 }

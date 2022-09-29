91eff3f8cfcb572d087631bd060326d0dafc6549
Pawel <62904551+Pawiszon15@users.noreply.github.com>
2022-09-29 18:55:39 +0200

    Slide setup

--- DoomishGame/Assets/Project/Scripts/PlayerV2/Slide.cs
+++ DoomishGame/Assets/Project/Scripts/PlayerV2/Slide.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Slide : MonoBehaviour
 {
+    [Header("Slide properties")]
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashTime;
+    [SerializeField] float invTime;
+    [SerializeField] float howMuchItChangePlayerSize;
+
+    [Header("References")]
+    [SerializeField] GameObject player;
+    [SerializeField] Rigidbody2D playerRb;
+
+    private ItemSwaper itemSwaper;
+    private ToolCooldown toolCooldown;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemSwaper = FindObjectOfType<ItemSwaper>();
+        toolCooldown = GetComponent<ToolCooldown>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Mouse1) && toolCooldown.rightMouseUse > 0)
+        {
+            //set spped greater than player movement with larp
+            //gradually decresse it 
+            StartCoroutine(SlideDash());
+        }
+    }
+
+    IEnumerator SlideDash()
+    {
+        yield return new WaitForSeconds(invTime);
+
+        yield return new WaitForSeconds(dashTime - invTime);
     }
 }
